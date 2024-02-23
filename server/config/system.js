579d2f424f3de1cd33f9b878c10f19497c4187f2
Mac Van Thanh <macthanhkmhd2003@gmail.com>
2024-02-23 15:42:38 +0700

    Update 23-02: Submmit on classroom

--- server/config/system.js
+++
@@ -1,5 +0,0 @@
-const PATH_ADMIN = "/admin";
-
-module.exports = {
-    prefixAdmin: PATH_ADMIN
-};
--- server/controllers/admin/dashboard.controller.js
+++
@@ -1,6 +0,0 @@
-
-module.exports.index = (req,res) =>{
-    res.render("admin/pages/dashboard/index",{
-        pageTitle: "Tổng quan"
-    });
-}
