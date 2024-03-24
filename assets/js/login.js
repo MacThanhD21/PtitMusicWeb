bb700a864d5ab5dff699b0e008bab2313c1304a4
Mac Van Thanh <macthanhkmhd2003@gmail.com>
2024-03-24 12:14:30 +0700

    Update 24-03: Auth...Temp...

--- assets/js/login.js
+++ assets/js/login.js
@@ -20,3 +20,96 @@ registerLink.addEventListener('click', function (event) {
   loginForm.classList.toggle('active');
   signUpForm.classList.toggle('active');
 });
+
+
+// const signUp = async (username, password) => {
+//   const response = await fetch('https://ap-southeast-1.aws.data.mongodb-api.com/app/data-pkcss/endpoint/register', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({ username, password }),
+//   });
+
+//   return response.json();
+// }
+
+// signUpForm.addEventListener('submit', async function (event) {
+//   event.preventDefault();
+//   const username = $('#username-signup').value;
+//   const password = $('#password-signup').value;
+
+//   const response = await signUp(username, password);
+//   console.log(response);
+
+//   if (response.status === 'success') {
+//     // Redirect to the login page
+//     signUpForm.classList.remove('active');
+//     loginForm.classList.add('active');
+//   }
+// }
+// );
+
+const usernameInput = $('#username');
+const passwordInput = $('#password');
+console.log(username, password);
+
+// Lắng nghe sự kiện khi người dùng nhập vào ô tên người dùng
+usernameInput.addEventListener('input', function (event) {
+  console.log(event.target.value);
+});
+
+// Lắng nghe sự kiện khi người dùng nhập vào ô mật khẩu
+passwordInput.addEventListener('input', function (event) {
+  console.log(event.target.value);
+});
+
+// Hàm đăng nhập
+const login = async (user, password) => {
+  try {
+    const response = await fetch('https://ap-southeast-1.aws.data.mongodb-api.com/app/data-pkcss/endpoint/validateauth', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ user, password }),
+    });
+
+    return response.json();
+  } catch (error) {
+    console.error('Error:', error);
+    throw new Error('Unable to login');
+  }
+};
+// Lắng nghe sự kiện submit của form đăng nhập
+loginForm.addEventListener('submit', async function (event) {
+  event.preventDefault(); // Ngăn chặn gửi form mặc định
+
+  const user = usernameInput.value;
+  const password = passwordInput.value;
+
+  try {
+    const response = await login(user, password);
+    console.log(response);
+    if (response && response.result._id) {
+      // Lưu token vào cookie
+      setCookie('accessToken', response.result._id, 1); // Thời gian sống của cookie là 7 ngày
+
+      // Chuyển hướng người dùng đến trang chính
+      window.location.href = '/index.html';
+    } else {
+      alert('Invalid username or password');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    alert('An error occurred while trying to login');
+  }
+});
+
+// Hàm thiết lập cookie
+function setCookie(name, value, days) {
+  const date = new Date();
+  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
