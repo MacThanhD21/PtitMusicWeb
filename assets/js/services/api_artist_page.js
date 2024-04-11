11bdaba04233028236fb7c695f0f9c2a408f8a1c
Mac Van Thanh <macthanhkmhd2003@gmail.com>
2024-04-11 21:26:36 +0700

    011-04:Complete...

--- assets/js/services/api_artist_page.js
+++ assets/js/services/api_artist_page.js
@@ -9,72 +9,61 @@ import { artists } from "../data/artists.js";
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const artistIdFromMainPage = urlParams.get("artistId");
+let listAlbumData = [];
 
-// console.log(artistIdFromMainPage);
-// Sử dụng albumId và albumName theo nhu cầu của bạn
+if (artistIdFromMainPage !== null) {
+  let artistFinded = artists.find(
+    (artist) => artist._id === artistIdFromMainPage
+  );
 
-let artistFinded = artists.find(
-  (artist) => artist._id === artistIdFromMainPage
-);
-// console.log(artistFinded);
-let albumFinded = artistFinded.id_albums; // laasy ra danh sach id cac album cua artist
+  // console.log(artistFinded);
+  let albumFinded = artistFinded.id_albums; // laasy ra danh sach id cac album cua artist
 
-async function getAlbumData(idAlbum) {
-  return await albums.find((album) => album._id === idAlbum);
-}
-
-async function getListAlbums() {
-  let listAlbumData = [];
-  for (const idAlbum of albumFinded) {
-    try {
-      const albumData = await getAlbumData(idAlbum);
-      if (albumData) {
-        listAlbumData.push(albumData);
-      } else {
-        console.log("Failed to fetch song data");
-      }
-    } catch (error) {
-      console.error("Error fetching song data:", error);
-    }
+  async function getAlbumData(idAlbum) {
+    return await albums.find((album) => album._id === idAlbum);
   }
-  return listAlbumData; // Trả về albumData khi đã hoàn thành vòng lặp
+
+  async function getListAlbums() {
+    for (const idAlbum of albumFinded) {
+      try {
+        const albumData = await getAlbumData(idAlbum);
+        if (albumData) {
+          listAlbumData.push(albumData);
+        } else {
+          console.log("Failed to fetch song data");
+        }
+      } catch (error) {
+        console.error("Error fetching song data:", error);
+      }
+    }
+    return listAlbumData; // Trả về albumData khi đã hoàn thành vòng lặp
+  }
+  listAlbumData = await getListAlbums(); // Lấy danh sách Album từ API
+} else {
+  listAlbumData = albums;
 }
-
-const listDataArray = await getListAlbums(); // Lấy danh sách Album từ API
-
-// console.log(listDataArray);
 
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-// const inForArtist = $("header .artist");
 const albumsOfArtist = $("#cardGridLen");
-// const header = $("header");
-// console.log(header);
-// console.log(imgArtist);
 
 const sectionPostHeader = $(".section-playlist-post-header");
-// console.log(sectionPostHeader);
-
-// header.style.cssText = `
-//   background: url(${imgArtist}) no-repeat center center/cover;
-//   object-fit: cover;
-//   background-size: cover;
-//   aspect-ratio: 16/9;
-// `;
-// console.log(inForArtist);
-// console.log(albumsOfArtist);
 
 // object app
 const app = {
   render__one: () => {
-    const html = `
+    try {
+      const html = `
     <h2>Albums</h2>
           <p>${artistFinded.name_Artist} • 2020-2024 • <span>${artistFinded.id_albums.length} Albums</span> </p>`;
-    sectionPostHeader.innerHTML = html;
+      sectionPostHeader.innerHTML = html;
+    } catch (error) {
+      console.log("Error when render__one", error);
+    }
   },
   render__two: () => {
-    listDataArray.forEach((album) => {
+    listAlbumData.forEach((album) => {
       const html = `
         <div class="card-simple">
           <a href="album.html?albumId=${album._id}">
