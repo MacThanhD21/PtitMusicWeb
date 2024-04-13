5eb54e135400d8fe76172db3267a671c3ec37f96
Mac Van Thanh <macthanhkmhd2003@gmail.com>
2024-04-13 10:15:50 +0700

    Update 13-04: updating Gemusic...

---
+++ assets/js/services/api_genmusic.js
@@ -0,0 +1,61 @@
+// Get Data from Suno API
+// Get data from input and send to Suno API
+// Get data from Suno API and display it on the page
+
+const axios = require("axios");
+
+// replace your vercel domain
+const baseUrl = "https://suno-api-lemon-tau.vercel.app/";
+
+async function customGenerateAudio(payload) {
+  const url = `${baseUrl}/api/custom_generate`;
+  const response = await axios.post(url, payload, {
+    headers: { "Content-Type": "application/json" },
+  });
+  return response.data;
+}
+
+async function generateAudioByPrompt(payload) {
+  const url = `${baseUrl}/api/generate`;
+  const response = await axios.post(url, payload, {
+    headers: { "Content-Type": "application/json" },
+  });
+  return response.data;
+}
+
+async function getAudioInformation(audioIds) {
+  const url = `${baseUrl}/api/get?ids=${audioIds}`;
+  const response = await axios.get(url);
+  return response.data;
+}
+
+async function getQuotaInformation() {
+  const url = `${baseUrl}/api/get_limit`;
+  const response = await axios.get(url);
+  return response.data;
+}
+
+async function main() {
+  const data = await generateAudioByPrompt({
+    prompt:
+      "A reggae-infused pop song with laid-back grooves and sunny melodies, bringing the feel-good vibes of the Caribbean islands.",
+    make_instrumental: false,
+    wait_audio: false,
+  });
+
+  const ids = `${data[0].id},${data[1].id}`;
+  console.log(`ids: ${ids}`);
+
+  for (let i = 0; i < 60; i++) {
+    const data = await getAudioInformation(ids);
+    if (data[0].status === "streaming") {
+      console.log(`${data[0].id} ==> ${data[0].audio_url}`);
+      console.log(`${data[1].id} ==> ${data[1].audio_url}`);
+      break;
+    }
+    // sleep 5s
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+  }
+}
+
+main();
